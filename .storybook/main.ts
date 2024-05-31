d0d69c53b6a770ab1862f2c6f771a14a63b0a6f8
panntod <pandhu.munjalindra@gmail.com>
2024-05-31 19:55:46 +0700

    feat: Configuration react storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,31 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
+
+// Konfigurasi Story Book
+const config: StorybookConfig = {
+
+  // Membaca semua file yang include ".stories"
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+
+  // Menambahkan plugin
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+
+  // Memberitahu config framework yang digunakan
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+
+  // Direktori yang static
+  staticDirs: ["..\\public"],
+};
+export default config;
