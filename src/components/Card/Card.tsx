aae7416cf9c74d69974565413fbd9b0b43dcc5c8
panntod <pandhu.munjalindra@gmail.com>
2024-05-31 19:58:14 +0700

    featL Create card component and documentation

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,27 @@
+import cn from '@/lib/clsx';
+import { FC, HTMLAttributes } from 'react';
+
+export interface CardProps extends HTMLAttributes<HTMLDivElement> {
+    variant?: 'elevation' | 'outlined';
+    backgroundColor?: string;
+    fontColor?: string;
+    classname?: string;
+    label: string;
+}
+
+export const Card: FC<CardProps> = ({ label, fontColor = '#000', backgroundColor = '#fff', variant = 'elevation', children, classname, ...rest }) => {
+    return (
+        <div
+            className={cn(
+                'rounded-xl p-2', 
+                variant === 'elevation' ? 'shadow-lg' : 'border-2 border-solid', 
+                classname
+            )}
+            {...rest}
+            style={{ backgroundColor, color: fontColor }}
+        >
+            {children}
+            <pre>{label}</pre>
+        </div>
+    );
+};
