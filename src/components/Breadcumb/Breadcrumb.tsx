88bc6a0a5ce090b1e50a0e6a0bad640d09b25407
panntod <pandhu.munjalindra@gmail.com>
2024-06-07 18:44:39 +0700

    refactor: Change code into semantic html

--- src/components/Breadcumb/Breadcrumb.tsx
+++ src/components/Breadcumb/Breadcrumb.tsx
@@ -1,30 +1,26 @@
-import { HTMLAttributes } from "react";
-import Link from "next/link";
-import { IconType } from "react-icons";
 import "./breadcrumb.css";
+import Link from "next/link";
+import { HTMLAttributes } from "react";
+
+import { BsHouse } from "react-icons/bs";
 
 export interface BreadcrumbProps extends HTMLAttributes<HTMLDivElement> {
     variant?: "large" | "medium" | "small";
-    data: { title: string; path: string; icon?: IconType }[];
+    data: { title: string; path: string; }[];
     separator?: string;
 }
 
-export const Breadcrumb = ({ variant = "medium", data, separator = ">", ...rest }: BreadcrumbProps) => {
+export const Breadcrumb = ({ variant = "medium", data, separator = ">" }: BreadcrumbProps) => {
     return (
-        <div className={[`flex`, `storybook-breadcrumb-${variant}`].join(" ")} {...rest}>
-            {data.map((item, index) => {
-                const IconComponent = item?.icon;
-
-                return (
-                    <span key={index} className="flex gap-2 items-center">
-                        {index > 0 && <span className="mx-2">{separator}</span>}
-                        {IconComponent && <IconComponent className="text-blue-500" />}
-                        <Link href={item.path} className={index === data.length - 1 ? "text-gray-500" : "text-blue-500"}>
-                            {item.title}
-                        </Link>
-                    </span>
-                );
-            })}
-        </div>
+        <ol className={[`flex`, `storybook-breadcrumb-${variant}`].join(" ")}>
+            {data.map((item, index) => (
+                <li key={index} className="flex gap-2 items-center">
+                    {index === 0 ? <BsHouse className="text-blue-500" /> : <span className="mx-2">{separator}</span>}
+                    <Link href={item.path} className={index === data.length - 1 ? "text-gray-500" : "text-blue-500"}>
+                        {item.title}
+                    </Link>
+                </li>
+            ))}
+        </ol>
     );
 };
