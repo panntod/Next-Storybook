920a591b1c818cb242da3b6ea63f44570a9ebca2
panntod <pandhu.munjalindra@gmail.com>
2024-06-07 19:02:06 +0700

    feat: Create component form modal

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -1,9 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Modal } from "./Modal";
 import { fn } from "@storybook/test";
+import { Button } from "../Button/Button";
 
 const meta: Meta<typeof Modal> = {
-    title: "Atomic/Modal",
+    title: "Molecule/Modal",
     component: Modal,
     parameters: {
         layout: "centered",
@@ -43,5 +44,40 @@ export const Dark: Story = {
         title: "Dark Theme",
         reject: "Decline",
         description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum aliquam magnam eum.",
+    },
+};
+
+export const Form: Story = {
+    args: {
+        variant: "dark",
+        title: "Login Form",
+        children: (
+            <div className="p-4 md:p-5">
+                <form className="space-y-4" action="#">
+                    <div>
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                        <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
+                    </div>
+                    <div>
+                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                        <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                    </div>
+                    <div className="flex justify-between">
+                        <div className="flex items-start">
+                            <div className="flex items-center h-5">
+                                <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+                            </div>
+                            <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+                        </div>
+                        <a href="#" className="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
+                    </div>
+
+                    <Button label="Submit" variant="primary" />
+                    <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+                        Not registered? <a href="#" className="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+                    </div>
+                </form>
+            </div>
+        ),
     },
 };
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -4,7 +4,7 @@ import { Button } from '../Button/Button';
 
 interface ModalProps {
     variant: "light" | "dark";
-    title?: string;
+    title: string;
     description?: string;
     accept: string;
     reject?: string;
@@ -54,28 +54,30 @@ export const Modal: React.FC<ModalProps> = ({
                             </button>
                         </header>
                         {children ?? (
-                            <section className="p-4 space-y-4">
-                                <p className={`text-base leading-relaxed ${variant === 'dark' ? 'text-gray-400' : 'text-gray-500'}`}>
-                                    {description}
-                                </p>
-                            </section>
+                            <>
+                                <section className="p-4 space-y-4">
+                                    <p className={`text-base leading-relaxed ${variant === 'dark' ? 'text-gray-400' : 'text-gray-500'}`}>
+                                        {description}
+                                    </p>
+                                </section>
+                                <footer className={`flex items-center p-4 border-t rounded-b ${variant === 'dark' ? 'border-gray-600' : 'border-gray-200'}`}>
+                                    <button
+                                        onClick={onClick}
+                                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                    >
+                                        {accept}
+                                    </button>
+                                    {reject &&
+                                        <button
+                                            onClick={toggleModal}
+                                            className={`py-2.5 px-5 ms-3 text-sm font-medium ${variant === 'dark' ? 'text-gray-400 bg-gray-800 border-gray-600 hover:bg-gray-700 hover:text-white' : 'text-gray-900 bg-white border-gray-200 hover:bg-gray-100 hover:text-blue-700'} focus:z-10 focus:ring-4 ${variant === 'dark' ? 'focus:ring-gray-700' : 'focus:ring-gray-100'} rounded-lg`}
+                                        >
+                                            {reject}
+                                        </button>
+                                    }
+                                </footer>
+                            </>
                         )}
-                        <footer className={`flex items-center p-4 border-t rounded-b ${variant === 'dark' ? 'border-gray-600' : 'border-gray-200'}`}>
-                            <button
-                                onClick={onClick}
-                                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                            >
-                                {accept}
-                            </button>
-                            {reject &&
-                                <button
-                                    onClick={toggleModal}
-                                    className={`py-2.5 px-5 ms-3 text-sm font-medium ${variant === 'dark' ? 'text-gray-400 bg-gray-800 border-gray-600 hover:bg-gray-700 hover:text-white' : 'text-gray-900 bg-white border-gray-200 hover:bg-gray-100 hover:text-blue-700'} focus:z-10 focus:ring-4 ${variant === 'dark' ? 'focus:ring-gray-700' : 'focus:ring-gray-100'} rounded-lg`}
-                                >
-                                    {reject}
-                                </button>
-                            }
-                        </footer>
                     </div>
                 </div>
             )}
