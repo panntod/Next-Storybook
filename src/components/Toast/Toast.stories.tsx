666361f3cdc01e35478ecd60b75e4bc63af57c1a
panntod <pandhu.munjalindra@gmail.com>
2024-08-06 07:09:11 +0700

    feat: Add autoclose toast

--- src/components/Toast/Toast.stories.tsx
+++ src/components/Toast/Toast.stories.tsx
@@ -1,72 +1,79 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Toast } from "./Toast";
-import { fn } from "@storybook/test";
 
 const meta: Meta<typeof Toast> = {
-    title: "Atomic/Toast",
-    component: Toast,
-    parameters: {
-        layout: "centered",
+  title: "Molecule/Toast",
+  component: Toast,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    variant: {
+      control: {
+        type: "select",
+        options: ["primary", "success", "warning", "danger"],
+      },
     },
-    tags: ["autodocs"],
-    argTypes: {
-        variant: {
-            control: {
-                type: "select",
-                options: ["primary", "success", "warning", "danger"],
-            },
-        },
-        size: {
-            control: {
-                type: "radio",
-                options: ["small", "medium", "large"],
-            },
-        },
-        onClick: { action: "clicked" },
+    size: {
+      control: {
+        type: "radio",
+        options: ["small", "medium", "large"],
+      },
     },
-    args: {
-        variant: "primary",
-        size: "medium",
-        message: "Toast",
-        onClick: fn(),
+    autoClose: {
+      control: {
+        type: "boolean",
+      },
     },
+    autoCloseTime: {
+      control: {
+        type: "number",
+      },
+    },
+  },
+  args: {
+    variant: "primary",
+    size: "medium",
+    message: "Toast",
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Toast>;
 
 export const Primary: Story = {
-    args: {
-        variant: "primary",
-        size: "medium",
-        title: "Info Message",
-        message: "This is Info message for information type",
-    },
+  args: {
+    variant: "primary",
+    size: "medium",
+    title: "Info Message",
+    message: "This is Info message for information type",
+  },
 };
 
 export const Success: Story = {
-    args: {
-        variant: "success",
-        size: "medium",
-        title: "Success Message",
-        message: "This is Success message for information type",
-    },
+  args: {
+    variant: "success",
+    size: "medium",
+    title: "Success Message",
+    message: "This is Success message for information type",
+  },
 };
 
 export const Warning: Story = {
-    args: {
-        variant: "warning",
-        size: "medium",
-        title: "Warning Message",
-        message: "This is Warning message for information type",
-    },
+  args: {
+    variant: "warning",
+    size: "medium",
+    title: "Warning Message",
+    message: "This is Warning message for information type",
+  },
 };
 
 export const Danger: Story = {
-    args: {
-        variant: "danger",
-        size: "medium",
-        title: "Danger Message",
-        message: "This is Danger message for information type",
-    },
+  args: {
+    variant: "danger",
+    size: "medium",
+    title: "Danger Message",
+    message: "This is Danger message for information type",
+  },
 };
--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,38 +1,69 @@
-import React from 'react';
+"use client";
+import React from "react";
 
 interface ToastProps {
-    variant: "primary" | "success" | "warning" | "danger";
-    size?: "small" | "medium" | "large";
-    title: string;
-    message: string;
-    onClick?: () => void;
+  variant: "primary" | "success" | "warning" | "danger";
+  size?: "small" | "medium" | "large";
+  title: string;
+  message: string;
+  autoClose?: boolean;
+  autoCloseTime?: number;
 }
 
 export const Toast: React.FC<ToastProps> = ({
-    variant = "primary",
-    size = "medium",
-    title,
-    message,
-    onClick,
+  variant = "primary",
+  size = "medium",
+  title,
+  message,
+  autoClose = true,
+  autoCloseTime = 5000,
 }) => {
-    const variants = {
-        success: "border-green-500 bg-green-100 text-green-600",
-        danger: "border-red-500 bg-red-100 text-red-600",
-        warning: "border-yellow-500 bg-yellow-100 text-yellow-600",
-        primary: "border-blye-500 bg-blue-100 text-blue-600",
-    }[variant]
+  const [isVisible, setIsVisible] = React.useState(false);
 
-    const sizes = {
-        small: "p-4",
-        medium: "p-5",
-        large: "p-6",
-    }[size]
+  const variants = {
+    success: "border-green-500 bg-green-100 text-green-600",
+    danger: "border-red-500 bg-red-100 text-red-600",
+    warning: "border-yellow-500 bg-yellow-100 text-yellow-600",
+    primary: "border-blue-500 bg-blue-100 text-blue-600",
+  }[variant];
 
-    return (
-        <div className={["border-2 rounded-lg relative", variants, sizes].join(" ")}>
-            <button onClick={onClick} type="button" className="absolute top-0 right-3 m-2 font-semibold">x</button>
-            <h1 className="text-xl font-semibold">{title}</h1>
-            <p className="text-base">{message}</p>
-        </div>
-    );
+  const sizes = {
+    small: "p-4",
+    medium: "p-5",
+    large: "p-6",
+  }[size];
+
+  React.useEffect(() => {
+    if (message) {
+      setIsVisible(true);
+
+      if (autoClose) {
+        const timer = setTimeout(() => {
+          setIsVisible(false);
+        }, autoCloseTime);
+
+        return () => clearTimeout(timer);
+      }
+    } else {
+      setIsVisible(false);
+    }
+  }, [message, autoClose, autoCloseTime]);
+
+  if (!isVisible) return null;
+
+  return (
+    <div
+      className={["border-2 rounded-lg relative", variants, sizes].join(" ")}
+    >
+      <button
+        onClick={() => setIsVisible(false)}
+        type="button"
+        className="absolute top-0 right-3 m-2 font-semibold"
+      >
+        x
+      </button>
+      <h1 className="text-xl font-semibold">{title}</h1>
+      <p className="text-base">{message}</p>
+    </div>
+  );
 };
